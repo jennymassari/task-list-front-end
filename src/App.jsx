2591fbd40f19ec9b52d825e8f6af80b26bc23a7f
Jenny Massari <jennymartins13@gmail.com>
2024-12-18 11:16:05 -0800

    Connected backend with frontend and started to make calls to backend.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,52 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import TaskList from './components/TaskList';
 import './App.css';
+import axios from 'axios';
 
-const TASKS = [
-  { id: 1, title: 'Mow the lawn', isComplete: false },
-  { id: 2, title: 'Cook Pasta', isComplete: true },
-];
+// const TASKS = [
+//   { id: 1, title: 'Mow the lawn', isComplete: false },
+//   { id: 2, title: 'Cook Pasta', isComplete: true },
+// ];
 
-const App = () => {
-  const [taskData, setTaskData] = useState(TASKS);
+const kbaseURL = 'http://127.0.0.1:5000';
+//const kbaseURL = 'http://localhost:5000';
+
+// function that makes the API call
+const getAllTasksApi = () => {
+  return axios.get(`${kbaseURL}/tasks`)
+    .then( response => {
+      return response.data;
+    })
+    .catch(error => {
+      console.log(error);
+    });
+};
+
+const taskApi = (id) => {
+  return axios.patch(`${kbaseURL}/tasks/${id}/mark_complete`)
+    .then( response => {
+      return response.data;
+    })
+    .catch(error => {
+      console.log(error);
+    });
+};
+
+function App (){
+  const [taskData, setTaskData] = useState([]);
+
+  //function to calls the function getAllTasksApi
+  const getAllTasks = () => {
+    getAllTasksApi()
+      .then(tasks => {
+        setTaskData(tasks);
+      });
+  };
+
+  // useEffect to call the function getAllTasks when the component mounts
+  useEffect(() => {
+    getAllTasks();
+  }, []);
 
   const handleToggleTask = (id) => {
     setTaskData((taskData) =>
